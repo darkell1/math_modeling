8697928e22832f2fe8453e8ef9c4cff551a49896
darkell1 <146851748+darkell1@users.noreply.github.com>
2023-10-16 17:12:11 +0000

    авыщптавргнпнпрвынрпнвыпн8гврнгпыв

--- lad/lab2task5.py
+++ lad/lab2task5.py
@@ -1,8 +1,10 @@
-x = int(input('Введите число 1'))
-y = int(input('Введите число 2'))
+x = int(input('Введите число 1: '))
+y = int(input('Введите число 2: '))
 
 if y == 0:
     print("Делитель равен 0, ответ 0")
 else:
-    print(x // y)
+    print('Частное:')
     print(x / y)
+    print('Остаток:')
+    print(x % y)
